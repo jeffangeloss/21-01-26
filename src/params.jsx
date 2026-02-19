0679626b181e6dce5a20851431b33949f2e95d1b
jeffangeloss <20235218@aloe.ulima.edu.pe>
2026-02-19 15:48:22 -0500

    le puse un / demás

--- src/params.jsx
+++ src/params.jsx
@@ -1,5 +1,5 @@
 const params = {
-    "BACKEND_URL": "https://backend-login-ha9h.onrender.com/"
+    "BACKEND_URL": "https://backend-login-ha9h.onrender.com"
     // "BACKEND_URL": "http://localhost:8000"
 }
 
